1b27005cc8f08559aec010142b7bc2da40cbd8a1
Aamleen Ahmed <aamleen20002@iiitd.ac.in>
2024-02-05 03:12:08 +0530

    Updated minor bug in evaluation and outputs

--- Task_2/extrinsic_evaluation.py
+++ Task_2/extrinsic_evaluation.py
@@ -14,17 +14,24 @@ with open('corpus.txt', 'r', encoding='utf-8') as file:
 with open('labels.txt', 'r', encoding='utf-8') as file:
     train_labels = file.readlines()
 
+# Remove newline characters from the loaded training labels
+for i in range(len(train_texts)):
+    train_texts[i] = train_texts[i].strip()
+    train_labels[i] = train_labels[i].strip()
+
+
 # Define emotions
 emotions = ['sadness', 'joy', 'love', 'anger', 'fear', 'surprise']
 
 # Function to load testing data
 def load_testing_data(emotion):
-    file_path = f'gen_{emotion}.txt'
+    file_path = f'sent_bigram_{emotion}.txt'
     with open(file_path, 'r', encoding='utf-8') as file:
         data = file.readlines()
     labels = [emotion] * len(data)
     texts = [' '.join(line.split()) for line in data]
     return labels, texts
+
 
 # Create the SVM model with TF-IDF vectorizer
 pipeline = Pipeline([
